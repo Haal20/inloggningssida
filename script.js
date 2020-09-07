8684f1a579f07ff5b4c554955693cf15d3e7f74b
Haal20 <69975021+Haal20@users.noreply.github.com>
2020-09-07 20:20:54 +0200

    råkade skriva meddelande för script i min index spara, haha

--- script.js
+++ script.js
@@ -10,15 +10,34 @@ let mainBody = document.getElementById("main-body");
 const userName = "test";
 const userPassword = "1234";
 
-mainButton.addEventListener("click", function lgIn() {
-  name = lginName.value;
-  password = lginPassword.value;
+//skapar funktioner till inloggningssidan
+function lgInPage() {
+  //Logga in
+  mainHeader.innerHTML = "Välkommen!";
+  mainBody.innerHTML = "Detta är min coola sida";
+  mainButton.innerHTML = "Logga ut";
+  document.getElementById("lginFail").innerHTML = "Du är inloggad!";
+  return;
+}
 
-  if (name === userName && password === userPassword) {
-    console.log(name + " is logged in!!!");
-    return;
-  } else {
-    console.log("Fel användarnamn eller lösenord. Försök igen...");
-    return;
-  }
-});
+function lgOutPage() {
+  //logga ut från sida
+  console.log("Gå tillbaka till första sidan...");
+  return;
+}
+
+function lgInFail() {
+  //om man skriver fel inlogg eller lösen
+  mainHeader.innerHTML = "Oj!";
+  mainBody.innerHTML = "...";
+  mainButton.innerHTML = "Försök igen";
+  document.getElementById("lginFail").innerText =
+    "Fel användarnamn eller lösenord...";
+  return;
+}
+
+function lgInRetry() {
+  //
+  console.log("Gå tillbaka till första sidan...");
+  return;
+}
