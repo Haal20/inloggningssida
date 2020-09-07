f195698f00e38d53e08c21f5810c5b6a71f63779
Haal20 <69975021+Haal20@users.noreply.github.com>
2020-09-07 16:38:16 +0200

    pseudokodar if() sats

--- script.js
+++ script.js
@@ -3,7 +3,25 @@ console.log("Hej Världen!");
 //skapar variabler till mina objekt på DOM
 let lginNamn = document.getElementById("lginNamn");
 let lginPassword = document.getElementById("lginPassword");
-let lginButton = document.getElementById("lginButton");
+let mainButton = document.getElementById("main-button");
+let mainHeader = document.getElementById("main-header");
+let mainBody = document.getElementById("main-body");
 //skapar två hårdkodade variabler som aggerar användarnamn och lösenord
 const userName = "test";
 const userPassword = "1234";
+
+mainButton.addEventListener("click", function lgInlgOut() {
+  /*
+    if (lginName.value === "test" && lginPassword.value === "1234") {
+        logga in mig
+    }
+    if (lginName.value !== "test" || lginPassword.value !== "1234") {
+        skriv ut "Felaktigt lösenord eller användarnamn... försök igen"
+    } 
+    if (i) {
+
+    } else {
+        skriv ut att det blev något fel på sidan
+    }
+*/
+});
