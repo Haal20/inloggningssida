dc36a38ee1176ab68032e694922575fe358540bc
Haal20 <69975021+Haal20@users.noreply.github.com>
2020-09-07 15:13:38 +0200

    skapar variabler till objekt på DOM och hårdkodade användarnamn och lösenord

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("Hej Världen!");
+
+//skapar variabler till mina objekt på DOM
+let lginNamn = document.getElementById("lginNamn");
+let lginPassword = document.getElementById("lginPassword");
+let lginButton = document.getElementById("lginButton");
+//skapar två hårdkodade variabler som aggerar användarnamn och lösenord
+const userName = "test";
+const userPassword = "1234";
