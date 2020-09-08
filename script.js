283430691af3a082241c5c13d79d31fee94702b0
Haal20 <69975021+Haal20@users.noreply.github.com>
2020-09-08 16:26:07 +0200

    got bort input som HTML text och skapade element med attribut direkt  i js

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 console.log("Hej Världen!");
 
+//skapar två input element
+let loginName = document.createElement("input");
+let loginPassword = document.createElement("input");
+//skapar input elementens attribut
+loginName.setAttribute("id", "loginName");
+loginName.setAttribute("onfocus", "this.value=''");
+loginName.setAttribute("type", "text");
+loginName.setAttribute("placeholder", "Skriv användarnamn...");
+//Skapar input elements attribut 2
+loginPassword.setAttribute("id", "loginPassword");
+loginPassword.setAttribute("onfocus", "this.value=''");
+loginPassword.setAttribute("type", "text");
+loginPassword.setAttribute("placeholder", "Skriv lösenord...");
+
 //skapar variabler till mina objekt på DOM
-let lginName = document.getElementById("lginName");
-let lginPassword = document.getElementById("lginPassword");
 let mainButton = document.getElementById("main-button");
 let mainHeader = document.getElementById("main-header");
 let mainBody = document.getElementById("main-body");
 let btnMessage = document.getElementById("btn-message");
-//skapar två hårdkodade variabler som aggerar användarnamn och lösenord
-const userName = "test";
-const userPassword = "1234";
 
 //skapar funktioner till inloggningssidan
-function lgInPage() {
+function welcomePage() {
+  //skapar två hårdkodade variabler som aggerar användarnamn och lösenord
   //skapa användare i localStorage
   //addToLocalStorage(userName, userPassword);
   //skapa sidan som inloggad
@@ -23,55 +33,57 @@ function lgInPage() {
   return;
 }
 
-function lgOutPage() {
-  //rensaLocalStorage
-  //localStorage.clear();
+function logInPage() {
   //skapa inloggningssidan igen
   mainHeader.innerHTML = "Logga in!";
-  mainBody.innerHTML = `
-  <input id="lginName" onfocus="this.Value=''" type="text" placeholder="Skriv användarnamn...">
-  <input id="lginPassword" onfocus="this.Value=''" type="text" placeholder="Skriv lösenord...">
-  `;
+  mainBody.appendChild(loginName);
+  mainBody.appendChild(loginPassword);
   mainButton.innerHTML = "Logga in";
+  btnMessage.innerHTML = " ";
   console.log("Gå tillbaka till första sidan...");
   return;
 }
 
-function lgInFail() {
+function errorPage() {
   //skapar en error sida
   mainHeader.innerHTML = "Oj!";
-  mainBody.innerHTML = "...";
+  mainBody.innerHTML = "";
   mainButton.innerHTML = "Försök igen";
   btnMessage.innerText = "Fel användarnamn eller lösenord...";
   return;
 }
 
-function lgInRetry() {
-  //skapa inloggningssidan igen
-  console.log("Gå tillbaka till första sidan...");
-  return;
-}
-
 //addEvent Listener på min knapp som tar mig till de olika sidorna
-mainButton.addEventListener("click", function lgIn() {
+mainButton.addEventListener("click", function logIn() {
+  //skapar två hårdkodade variabler som aggerar användarnamn och lösenord
+  const userName = "test";
+  const userPassword = "1234";
   //Läser in användarens input
-  name = lginName.value;
-  password = lginPassword.value;
+  name = loginName.value;
+  password = loginPassword.value;
 
-  //olika vägar beroende på input
-  if (mainButton.innerText === "Försök igen") {
-    //om man skriver fel lösen eller och vill försöka igen
-    lgInRetry();
+  console.log("Syns jag här?");
+
+  if (mainButton.innerHTML === "Försök igen") {
+    //tar en till första sidan från Error sidan
+    logInPage();
+    return;
   }
-  if (mainButton.innerText === "Logga ut") {
-    //on man vill logga ut från sidan
-    lgOutPage();
+  if (mainButton.innerHTML === "Logga ut") {
+    //Tar en till första sidan från logga in sidan
+    logInPage();
+    return;
   }
-  if (name === userName && password === userPassword) {
+  if (mainButton === "Logga in") {
+    console.log("Syns jag här?  if()");
+
     //om man skriver rätt lösenord
-    lgInPage();
+    if (name === userName && password === userPassword) {
+      welcomePage();
+      return;
+    }
   } else {
-    //om man skriver fel lösenord
-    lgInFail();
+    //Om man skriver fel inlogg
+    errorPage();
   }
 });
